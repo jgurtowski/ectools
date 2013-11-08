407eb7cc74d53d53dcb2171ed288551904944527
james <gurtowsk@mshadoop1.cshl.edu>
2013-11-08 14:41:43 -0500

    join will now ignore fields if not in db using param

--- join.py
+++ join.py
@@ -5,8 +5,8 @@
 import sys
 from itertools import imap
 
-if not len(sys.argv) == 3:
-    print "join.py db file.txt"
+if not len(sys.argv) >= 3 :
+    print "join.py db file.txt [leave missing(true,false)]"
     sys.exit(1)
 
 dbfh = open(sys.argv[1])
@@ -14,13 +14,19 @@ sys.stderr.write("Loading db ...\n")
 db = dict(imap(lambda line: (line.strip().split()[0],line.strip().split()[1:]),dbfh))
 sys.stderr.write("done\n")
 
+ignore = False
+if(len(sys.argv) > 3):
+    ignore = "true" == sys.argv[3]
+
 infh = open(sys.argv[2])
 
 for line in infh:
     arr = line.strip().split()
     key = arr[0]
     if not db.has_key(key):
-        sys.stderr.write("Key not in db: %s\n" % key) 
+        sys.stderr.write("Key not in db: %s\n" % key)
+        if ignore:
+            print line.strip()
         continue
     print "\t".join(db[key]+arr[1:])
 
