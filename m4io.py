f8f9365bae8c977b5a9c9348982387b2a52f2738
james <gurtowsk@mshadoop1.cshl.edu>
2014-05-23 12:28:22 -0400

    functions for looking at m4 alignments

---
+++ m4io.py
@@ -0,0 +1,45 @@
+
+from operator import itemgetter,attrgetter
+from collections import namedtuple
+from itertools import groupby, imap
+from nucio import fileIterator, lineRecordIterator
+from nucio import M4Record, M4RecordTypes
+from misc import identityFunc
+
+
+def getRawAlignments(fn):
+    '''fn is the filename
+       returns An iterator over raw M4Records'''
+    itemIterator = lambda f : lineRecordIterator(f, M4Record, M4RecordTypes)
+    return fileIterator(fn, itemIterator)
+
+def getAlignments(fn, filter_func=identityFunc):
+    '''fn is the filename 
+       returns alignments grouped by their name
+       filter_func works on a group of alignments for a specific read
+    '''
+    raw_alignments = getRawAlignments(fn)
+    groupby_first = itemgetter(0)
+    grouped = groupby(raw_alignments,groupby_first)
+    return imap(lambda (k,v) : (k,filter_func(list(v))), grouped)
+
+
+def filterLongest(alignments):
+    ''' Get the longest alignment '''
+    align_len = lambda r : r.qend - r.qstart
+    return max(alignments,key=align_len)
+
+def filterBestScore(alignments):
+    '''Get the best scoring alignment'''
+    score_getter = attrgetter("score")
+    return min(alignments,key=score_getter)
+
+def longestNonOverlapping(alignments):
+    '''Find the longest non overlapping alignments'''
+    align_len = lambda r : r.qend - r.qstart
+    alignments.sort(key=align_len, reverse=True)
+    longest = alignments[0]
+    not_overlaps = lambda y : y.qend < longest.qstart or y.qstart > longest.qend
+    newset = [longest] + filter(not_overlaps, alignments)
+    return newset
+    
