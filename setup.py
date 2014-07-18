7bc393f2a07e5c9cd3662c113489d88f2af1c823
james <gurtowsk@mshadoop1.cshl.edu>
2014-07-18 10:27:33 -0400

    typo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 setup(name='ectools',
       version='0.2',
       description='Long Read Error Correction using pre-assembled short reads',
-      long_description=readmine(),
+      long_description=readme(),
       url='https://github.com/jgrutowski/ectools',
       author='James Gurtowski',
       author_email='gurtowsk@cshl.edu',
